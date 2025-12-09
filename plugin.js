4eb4da95bedab47453ddaddb96dba9804c597f1f
Sovkus <sovkus@gmail.com>
2025-12-09 13:00:57 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,25 +1,31 @@
-// Проверяем, есть ли module и exports, если нет, то создаем их
-if (typeof module === 'undefined') {
-  var module = { exports: {} };
-}
-
-if (typeof exports === 'undefined') {
-  var exports = module.exports;
-}
-
-module.exports = {
-  create: function() {
-    console.log('Image Editor plugin: creating...');
-    
-    return {
-      html: '<div style="padding: 20px;"><h1>Image Editor Test</h1><p>If you see this, plugin is working!</p></div>',
-      css: 'div { color: var(--penpot-text-primary); }',
-      onMount: function(root) {
-        console.log('Plugin mounted!', root);
-      },
-      onMessage: function(data) {
-        console.log('Message received:', data);
-      }
-    };
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.PenpotPlugin = factory();
   }
-};
+}(typeof self !== 'undefined' ? self : this, function () {
+  return {
+    create: function() {
+      console.log('Image Editor plugin: creating...');
+      
+      return {
+        html: '<div style="padding: 20px;"><h1>Image Editor Test</h1><p>If you see this, plugin is working!</p></div>',
+        css: 'div { color: var(--penpot-text-primary); }',
+        onMount: function(root) {
+          console.log('Plugin mounted!', root);
+        },
+        onMessage: function(data) {
+          console.log('Message received:', data);
+        }
+      };
+    }
+  };
+}));
