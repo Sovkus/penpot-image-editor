7e6cde123ce498c8fae837ad19330ca34d605998
Sovkus <sovkus@gmail.com>
2025-12-09 14:38:25 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,17 +1,2 @@
-// Минимальный рабочий плагин для Penpot
-window.PenpotPlugin = {
-    create: function() {
-        console.log('Image Editor: create() called');
-        
-        return {
-            html: '<div style="padding: 20px; color: var(--penpot-text-primary);">Image Editor Plugin - Working!</div>',
-            css: '',
-            onMount: function(root) {
-                console.log('Plugin mounted!', root);
-            },
-            onMessage: function(data) {
-                console.log('Message:', data);
-            }
-        };
-    }
-};
+// Простейший плагин для Penpot - только вывод в консоль
+console.log('=== PENPOT PLUGIN: simple.js загружен ===');
