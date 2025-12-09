7629297efd05e2f44b5e7b173b3d081eb9ed8f91
Sovkus <sovkus@gmail.com>
2025-12-09 13:02:20 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,31 +1,17 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory();
-  } else {
-    // Browser globals (root is window)
-    root.PenpotPlugin = factory();
-  }
-}(typeof self !== 'undefined' ? self : this, function () {
-  return {
-    create: function() {
-      console.log('Image Editor plugin: creating...');
-      
-      return {
-        html: '<div style="padding: 20px;"><h1>Image Editor Test</h1><p>If you see this, plugin is working!</p></div>',
-        css: 'div { color: var(--penpot-text-primary); }',
-        onMount: function(root) {
-          console.log('Plugin mounted!', root);
-        },
-        onMessage: function(data) {
-          console.log('Message received:', data);
-        }
-      };
-    }
+// Минимальный пример для Penpot с использованием exports
+if (typeof exports !== 'undefined') {
+  exports.create = function() {
+    console.log('Image Editor plugin: creating...');
+    
+    return {
+      html: '<div style="padding: 20px;"><h1>Image Editor Test</h1><p>If you see this, plugin is working!</p></div>',
+      css: 'div { color: var(--penpot-text-primary); }',
+      onMount: function(root) {
+        console.log('Plugin mounted!', root);
+      },
+      onMessage: function(data) {
+        console.log('Message received:', data);
+      }
+    };
   };
-}));
+}
